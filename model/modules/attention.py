354f02e5bad4b2cc01a23c2b32ea684ecd14de8b
RadicallyUprooted <minakami1984@gmail.com>
2025-07-09 16:40:17 +0000

    change positional encoding to relative positional encoding

--- model/modules/attention.py
+++ model/modules/attention.py
@@ -5,67 +5,85 @@ from torch import Tensor
 from torch import nn
 from einops import rearrange, einsum
 
-class PositionalEncoding(nn.Module):
+class RelativePositionalEncoding(nn.Module):
 
     def __init__(
         self,
-        encoder_dim: int,
+        d_model: int,
     ):
-        super(PositionalEncoding, self).__init__()
+        super(RelativePositionalEncoding, self).__init__()
 
-        self.encoder_dim = encoder_dim
+        self.d_model = d_model
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, q: Tensor) -> Tensor:
         
-        x_length = x.size(1)
+        q_length = q.size(2)
+        
+        range_vec = torch.arange(q_length)
+        range_mat = rearrange(range_vec, 'j -> 1 j')
 
-        encodings = torch.zeros(x_length, self.encoder_dim)    # (x_length, encoder_dim)
-        positions = torch.arange(0, x_length).unsqueeze(1)      # (x_length, 1)
+        distance_mat = range_mat - rearrange(range_vec, 'i -> i 1')
+        
+        distance_mat_clipped = torch.clamp(distance_mat, -30, 30) # TODO: make clip value a parameter
+        
+        final_mat = distance_mat_clipped + 30 # TODO: make clip value a parameter
+
+        encodings = torch.zeros(2 * 30 + 1, self.d_model)    # (x_length, d_model)
+        positions = torch.arange(0, (2 * 30 + 1)).unsqueeze(1)      # (x_length, 1)
 
         div_term = torch.exp(
-            torch.arange(0, self.encoder_dim, 2) * -(math.log(10000.0) / self.encoder_dim)
+            torch.arange(0, self.d_model, 2) * -(math.log(10000.0) / self.d_model)
         )
         
         encodings[:, 0::2] = torch.sin(positions * div_term)
         encodings[:, 1::2] = torch.cos(positions * div_term)
-        encodings = rearrange(encodings, 't f -> 1 t f').requires_grad_(False)
-        return encodings.to(x.device)
+        encodings = encodings[final_mat]
+        encodings = rearrange(encodings, 't1 t2 f -> 1 t1 t2 f').requires_grad_(False)
+        return encodings.to(q.device)
 
 class MultiHeadSelfAttention(nn.Module):
 
     def __init__(
         self,
-        encoder_dim: int,
-        attention_heads: int,
-        dropout_p: float,
+        d_model: int,
+        n_heads: int,
+        dropout: float,
     ):
         super(MultiHeadSelfAttention, self).__init__()
 
-        self.heads_dim = encoder_dim // attention_heads
-        self.attention_heads = attention_heads
+        self.heads_dim = d_model // n_heads
+        self.attention_heads = n_heads
 
-        self.q_proj = nn.Linear(encoder_dim, encoder_dim)
-        self.k_proj = nn.Linear(encoder_dim, encoder_dim)
-        self.v_proj = nn.Linear(encoder_dim, encoder_dim)
-        self.pos_proj = nn.Linear(encoder_dim, encoder_dim)
+        self.q_proj = nn.Linear(d_model, d_model)
+        self.k_proj = nn.Linear(d_model, d_model)
+        self.v_proj = nn.Linear(d_model, d_model)
+        self.pos_proj = nn.Linear(d_model, d_model)
 
-        self.dropout = nn.Dropout(p=dropout_p)
+        self.dropout = nn.Dropout(p=dropout)
 
-        self.out_proj = nn.Linear(encoder_dim, encoder_dim)
+        self.out_proj = nn.Linear(d_model, d_model)
+        
+        self.u_bias = nn.Parameter(torch.zeros(1, self.attention_heads, 1, self.heads_dim))
+        self.v_bias = nn.Parameter(torch.zeros(1, self.attention_heads, 1, self.heads_dim))
 
     def forward(
-        self, x: Tensor,
-        pos_embeddings: Tensor
+        self, 
+        inputs: Tensor,
+        relative_positional_embeddings: Tensor
     ) -> Tensor:
 
-        q = rearrange(self.q_proj(x), 'b t (h d) -> b h t d', h=self.attention_heads)
-        k = rearrange(self.k_proj(x), 'b t (h d) -> b h t d', h=self.attention_heads)
-        v = rearrange(self.v_proj(x), 'b t (h d) -> b h t d', h=self.attention_heads)
+        q = rearrange(self.q_proj(inputs), 'b t (h d) -> b h t d', h=self.attention_heads)
+        k = rearrange(self.k_proj(inputs), 'b t (h d) -> b h t d', h=self.attention_heads)
+        v = rearrange(self.v_proj(inputs), 'b t (h d) -> b h t d', h=self.attention_heads)
         
-        pos = rearrange(self.pos_proj(pos_embeddings), 'b t (h d) -> b h t d', h=self.attention_heads)
+        pos = self.pos_proj(relative_positional_embeddings)
+        
+        q_with_bias_u = q + self.u_bias
+        q_with_bias_v = q + self.v_bias
 
-        qk_score = einsum(q, k, 'b h t1 d, b h t2 d -> b h t1 t2')
-        pos_score = einsum(q, pos, 'b h t1 d, b h t2 d -> b h t1 t2')
+        qk_score = einsum(q_with_bias_u, k, 'b h t1 d, b h t2 d -> b h t1 t2')
+        
+        pos_score = einsum(q_with_bias_v, pos, 'b h t1 d, b t1 t2 d -> b h t1 t2')
 
         scores = (qk_score + pos_score) / math.sqrt(self.heads_dim)
 
@@ -83,23 +101,23 @@ class Attention(nn.Module):
 
     def __init__(
         self,
-        encoder_dim: int,
-        attention_heads: int,
-        dropout_p: float,
+        d_model: int,
+        n_heads: int,
+        dropout: float,
     ):
         super(Attention, self).__init__()
 
-        self.layer_norm = nn.LayerNorm(encoder_dim)
-        self.pos_embeddings = PositionalEncoding(encoder_dim)
-        self.attn = MultiHeadSelfAttention(encoder_dim, attention_heads, dropout_p)
-        self.dropout = nn.Dropout(p=dropout_p)
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.pos_embeddings = RelativePositionalEncoding(d_model)
+        self.attn = MultiHeadSelfAttention(d_model, n_heads, dropout)
+        self.dropout = nn.Dropout(p=dropout)
 
-    def forward(self, x: Tensor) -> Tensor:
-
-        pos_emb = self.pos_embeddings(x)
+    def forward(self, inputs: Tensor) -> Tensor:
         
-        x = self.layer_norm(x)
-        x = self.attn(x, pos_emb)
-        x = self.dropout(x)
+        inputs = self.layer_norm(inputs)
+        q = rearrange(self.attn.q_proj(inputs), 'b t (h d) -> b h t d', h=self.attn.attention_heads)
+        relative_positional_embedding = self.pos_embeddings(q)
+        outputs = self.attn(inputs, relative_positional_embedding)
+        outputs = self.dropout(outputs)
 
-        return x
+        return outputs
