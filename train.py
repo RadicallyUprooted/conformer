a274c5ed09201c41628775b1cb8c8e6e27de346f
RadicallyUprooted <minakami1984@gmail.com>
2025-07-13 13:37:41 +0000

    update configs

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torchmetrics.text import CharErrorRate, WordErrorRate
 from model.conformer import Conformer
-from data.dataset import LibriSpeechDataModule
+from data.custom_dataloader import DataModule
 from text_processor.processor import CharTextTransform
 
 class ConformerLightningModule(pl.LightningModule):
@@ -18,26 +18,27 @@ class ConformerLightningModule(pl.LightningModule):
     A LightningModule that encapsulates the Conformer model, training logic,
     and optimizer configuration.
     """
-    def __init__(self, model_cfg: DictConfig, train_cfg: DictConfig, data_cfg: DictConfig, optimizer_cfg: DictConfig, decoder_cfg: DictConfig):
+    def __init__(self, cfg: DictConfig):
         super().__init__()
         self.save_hyperparameters()
+        self.cfg = cfg
         self.text_transform = CharTextTransform()
         self.model = Conformer(
-            vocab_size=data_cfg.vocab_size,
-            n_layers=model_cfg.n_layers,
-            n_mels=data_cfg.n_mels,
-            d_model=model_cfg.d_model,
-            n_heads=model_cfg.n_heads,
-            conv_kernel_size=model_cfg.conv_kernel_size,
-            dropout=model_cfg.dropout,
+            vocab_size=self.cfg.data.vocab_size,
+            n_mels=self.cfg.data.n_mels,
+            n_layers=self.cfg.model.n_layers,
+            d_model=self.cfg.model.d_model,
+            n_heads=self.cfg.model.n_heads,
+            conv_kernel_size=self.cfg.model.conv_kernel_size,
+            dropout=self.cfg.model.dropout,
         )
 
         self.criterion = nn.CTCLoss(blank=self.text_transform.blank, zero_infinity=True)
-        self.learning_rate = train_cfg.learning_rate
-        self.decoder = hydra.utils.instantiate(decoder_cfg, text_transform=self.text_transform, blank='<blank>')
+        self.learning_rate = self.cfg.train.learning_rate
+        self.decoder = hydra.utils.instantiate(self.cfg.decoder, text_transform=self.text_transform, blank='<blank>')
         self.cer = CharErrorRate()
         self.wer = WordErrorRate()
-        self.optimizer_cfg = optimizer_cfg
+        self.optimizer_cfg = self.cfg.optimizer
 
     def forward(self, inputs, input_lengths):
         return self.model(inputs, input_lengths)
@@ -90,14 +91,13 @@ class ConformerLightningModule(pl.LightningModule):
 
             self.logger.experiment.log({
                 "examples": [
-                    wandb.Audio(waveform.numpy(), caption=f"Pred: {pred_text} | GT: {ground_truth_text}", sample_rate=16000)
+                    wandb.Audio(waveform.squeeze().numpy(), caption=f"Pred: {pred_text} | GT: {ground_truth_text}", sample_rate=16000)
                 ]
             })
 
         return loss
     
     def on_validation_epoch_end(self):
-        # Compute and log average WER and CER
         avg_wer = self.wer.compute()
         avg_cer = self.cer.compute()
         
@@ -114,32 +114,15 @@ class ConformerLightningModule(pl.LightningModule):
 @hydra.main(config_path="configs", config_name="config", version_base=None)
 def main(cfg: DictConfig):
 
-    data_module = LibriSpeechDataModule(
-        path=cfg.data.path,
-        train_url=cfg.data.train_url,
-        val_url=cfg.data.val_url,
-        batch_size=cfg.train.batch_size, 
-        n_mels=cfg.data.n_mels, 
-        vocab_size=cfg.data.vocab_size,
-        time_mask_param=cfg.data.time_mask_param,
-        freq_mask_param=cfg.data.freq_mask_param,
-        num_workers=cfg.data.num_workers,
-    )
-
-    model_module = ConformerLightningModule(
-        model_cfg=cfg.model,
-        train_cfg=cfg.train,
-        data_cfg=cfg.data,
-        optimizer_cfg=cfg.optimizer,
-        decoder_cfg=cfg.decoder,
-    )
+    data_module = DataModule(cfg=cfg)
+    model_module = ConformerLightningModule(cfg=cfg)
 
     wandb_logger = WandbLogger(project="conformer", name="conformer-run")
 
     checkpoint_callback = ModelCheckpoint(
         dirpath="checkpoints/",
         filename="conformer-{epoch:02d}",
-        every_n_epochs=cfg.train.epochs,
+        every_n_epochs=10,
     )
 
     trainer = pl.Trainer(
